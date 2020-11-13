4d1b39adf2253e26dbe3393ceb42dd54017f5e5f
shivam-sood <soodshivam29@gmail.com>
2020-11-13 23:24:17 +0530

    Small changes

---
+++ python.py
@@ -0,0 +1,15 @@
+import math
+import os
+from collections import namedtuple
+from flask import Flask
+import sys
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug = true)
